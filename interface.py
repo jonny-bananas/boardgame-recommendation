d826b14ed80733069db10dac5b0e19c09718200a
Jonathan David-Jackson <davidjaj@oregonstate.edu>
2024-02-11 19:47:48 -0800

    added missing search functionality

--- interface.py
+++ interface.py
@@ -1,13 +1,28 @@
 import inquirer
+import csv
+import re
 from pprint import pprint
 from clint.textui import puts, indent, colored
 
+BOARDGAME_LIST = 'boardgames_ranks.csv'
+
+# removes spaces, capitals, and special characters
+def normalize_text(text):
+    return re.sub(r'\W+', '', text).lower() 
 
 def search_by_genre(genre):
     pass
 
-def search_by_name(game_name):
-    pass
+def search_by_name(u_input, BOARDGAME_LIST):
+    normy_input = normalize_text(u_input)
+    with open(BOARDGAME_LIST, newline= '', encoding= 'utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if(normalize_text(row['name']) == normy_input):
+                return row
+
+    return None
+
 
 def random_game():
     pass
@@ -33,28 +48,36 @@ def main():
     ]
 
     while True:
-        user_choice = inquirer.prompt(options)['options']
+        user_choice = inquirer.prompt(options)
 
         if user_choice == {'options': 'Search for a game by genre'}:
-            genre = input("Enter the name of a game you enjoy: ")
+            genre = input("What kind of game would you like to look up: ")
             search_by_genre(genre)
-            break
 
         elif user_choice == {'options': 'Search for a game by name'}:
             game_name = input("Enter the name of any Board Game: ")
-            search_by_name(game_name)
-            break
+            result = search_by_name(game_name, BOARDGAME_LIST)
+
+            if result:
+                print("\n")
+                puts(colored.red("Is this the game you want: "))
+                for key, value in result.items():
+                    print(f"{key}: {value}")
+                print("\n")
+            else:
+                print("Hm, that's odd. We can't find that game. Please try again.")
 
         elif user_choice == {'options': 'Recommend a random game'}:
-            break
+            random_game()
 
         elif user_choice == {'options': 'Quit'}:
+            print("------------------------------------------------------------------------------------------")
+            puts(colored.red("Thanks for using my tool. I hope you've found it helpful!"))
+            print("------------------------------------------------------------------------------------------")
             break
-
         else:
             print("This is an invalid option. Please try again...")
-
-    print("\n\n")
+    print("\n")
 
 
 
